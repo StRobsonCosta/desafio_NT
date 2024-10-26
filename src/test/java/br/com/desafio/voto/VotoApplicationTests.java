5ab12bc4f9c44c8f05fc61a03ebac9470ef1c04e
Robson Costa <st.robson.costa@gmail.com>
2024-10-26 17:44:46 -0300

    FIX: CI

--- src/test/java/br/com/desafio/voto/VotoApplicationTests.java
+++ src/test/java/br/com/desafio/voto/VotoApplicationTests.java
@@ -3,7 +3,7 @@ package br.com.desafio.voto;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=")
 class VotoApplicationTests {
 
 	@Test
